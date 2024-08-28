a7f98fc35dd0f2baebe9bca7cd8be7c662147a38
Allwin <allwin@cognultsoft.com>
2024-08-28 22:24:15 +0530

    updated the active style

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -170,6 +170,7 @@ const Header = (props: any) => {
                                 >
                                     {item?.label}
                                 </Link>
+                                {(item?.status === 'active') && (<span className='w-[5px] h-[1px] bg-black mt-[4px]'></span>)}
                             </li>
                         );
                     }
