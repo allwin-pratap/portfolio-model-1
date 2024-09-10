d32205f53f2378f381552cf9c29346659dd12a5d
Allwin <allwin@cognultsoft.com>
2024-09-10 20:45:17 +0530

    implemented s3 image

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['mr-portfolio.s3.eu-north-1.amazonaws.com'],
+  },
   compiler: {
     styledComponents: true,
-  }
+  },
 };
 
 export default nextConfig;
