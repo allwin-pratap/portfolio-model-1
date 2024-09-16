f9c4a0e4186d8aabf4c18161773db789452aaecf
Allwin <allwin@cognultsoft.com>
2024-09-16 22:31:47 +0530

    implemented the hover effect

--- src/templates/Works/ImageGrid/index.tsx
+++ src/templates/Works/ImageGrid/index.tsx
@@ -10,17 +10,15 @@ export default function AllWorks(props: any) {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[30px] mt-[50px]">
                 {props?.images?.map((works: any, index: any) => {
                     return (
-                        // <a key={index} className={``} href={works.url}>
-                            <div key={index} className={`mx-auto max-w-[405px] max-h-[285px] rounded-[25px] overflow-hidden`}>
-                                <ImageLoader
-                                    src={StaticPath(works.img)}
-                                    className="transition-all duration-[300ms] ease-in-out scale-[1] hover:scale-[1.05]"
-                                    alt={works.img_alt}
-                                    width={405}
-                                    height={285}
-                                />
-                            </div>
-                        // </a>
+                        <div key={index} className={`mx-auto max-w-[405px] max-h-[285px] rounded-[25px] overflow-hidden`}>
+                            <ImageLoader
+                                src={StaticPath(works.img)}
+                                className="transition-all duration-[300ms] ease-in-out scale-[1] hover:scale-[1.05]"
+                                alt={works.img_alt}
+                                width={405}
+                                height={285}
+                            />
+                        </div>
                     )
                 })}
             </div>
