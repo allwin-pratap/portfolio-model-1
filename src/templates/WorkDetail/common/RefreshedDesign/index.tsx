480091a831dd5708ba2ce6fde4efbdd1da2a2df9
Allwin <allwin@cognultsoft.com>
2024-09-26 00:24:12 +0530

    updated the reiki page

--- src/templates/WorkDetail/common/RefreshedDesign/index.tsx
+++ src/templates/WorkDetail/common/RefreshedDesign/index.tsx
@@ -14,37 +14,35 @@ export default function RefreshedDesign(props: any) {
                 {data?.designs?.map((design: any, index: any) => {
                     return (
                         <React.Fragment key={index}>
-                            <AnimatedWrapper>
-                                <div key={index} className="pb-[80px] last:pb-0">
-                                    <div className="max-w-[600px] mx-auto">
-                                        <p className={`text-[14px] leading-[21px] font-[400] text-[#595959] text-center`}>{design?.tag}</p>
-                                        <p className={`text-[36px] leading-[42px] font-[400] text-black text-center pt-[20px] pb-[10px]`}>{design?.title}</p>
-                                        <p className={`text-[20px] leading-[30px] font-[300] text-[#595959] text-center`}>{design?.description}</p>
-                                    </div>
-                                    <div className={`mt-[60px] border border-[#F1F7FF] rounded-[25px] p-[35px] md:p-[55px] lg:p-[70px] ${styles[`${design?.bg_color}`]}`}>
-                                        {
-                                            (design?.is_behance) ? (
-                                                <div className='flex flex-col items-center'>
-                                                    <p className='text-[20px] leading-[30px] font-[400] text-black text-center pb-[30px]'>{design?.behance_data?.desc}</p>
-                                                    <Link
-                                                        href={design?.behance_data?.btn_url}
-                                                        className={`text-[20px] leading-[30px] font-[400] text-white p-[15px] bg-black rounded-[55px]`}
-                                                        target={`_blank`}>{design?.behance_data?.btn_label}
-                                                    </Link>
-                                                </div>
-                                            ) : (
-                                                <div className="max-w-[815px] mx-auto flex justify-center items-center">
-                                                    <ImageLoader
-                                                        className="rounded-[25px]"
-                                                        src={StaticPath(design?.img)}
-                                                        alt={design?.img_alt}
-                                                        width={design?.width ?? 815}
-                                                        height={design?.height ?? 580}
-                                                    />
-                                                </div>
-                                            )
-                                        }
-                                    </div>
+                            <AnimatedWrapper customStyle={`pb-[80px] last:pb-0`}>
+                                <div className="max-w-[600px] mx-auto">
+                                    <p className={`text-[14px] leading-[21px] font-[400] text-[#595959] text-center`}>{design?.tag}</p>
+                                    <p className={`text-[36px] leading-[42px] font-[400] text-black text-center pt-[20px] pb-[10px]`}>{design?.title}</p>
+                                    <p className={`text-[20px] leading-[30px] font-[300] text-[#595959] text-center`}>{design?.description}</p>
+                                </div>
+                                <div className={`mt-[60px] border border-[#F1F7FF] rounded-[25px] p-[35px] md:p-[55px] lg:p-[70px] ${styles[`${design?.bg_color}`]}`}>
+                                    {
+                                        (design?.is_behance) ? (
+                                            <div className='flex flex-col items-center'>
+                                                <p className='text-[20px] leading-[30px] font-[400] text-black text-center pb-[30px]'>{design?.behance_data?.desc}</p>
+                                                <Link
+                                                    href={design?.behance_data?.btn_url}
+                                                    className={`text-[20px] leading-[30px] font-[400] text-white p-[15px] bg-black rounded-[55px]`}
+                                                    target={`_blank`}>{design?.behance_data?.btn_label}
+                                                </Link>
+                                            </div>
+                                        ) : (
+                                            <div className="max-w-[815px] mx-auto flex justify-center items-center">
+                                                <ImageLoader
+                                                    className="rounded-[25px]"
+                                                    src={StaticPath(design?.img)}
+                                                    alt={design?.img_alt}
+                                                    width={design?.width ?? 815}
+                                                    height={design?.height ?? 580}
+                                                />
+                                            </div>
+                                        )
+                                    }
                                 </div>
                             </AnimatedWrapper>
                         </React.Fragment>
